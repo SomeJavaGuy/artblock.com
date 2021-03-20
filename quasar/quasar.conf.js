10fc9c8928b9377607cf7fe91e1fba370f188a29
Alex Astrum <alexastrum@gmail.com>
2021-03-20 12:59:32 -0400

    Created Firebase project

--- quasar/quasar.conf.js
+++ quasar/quasar.conf.js
@@ -53,6 +53,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
+      distDir: '../firebase/public/eth-demo',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
